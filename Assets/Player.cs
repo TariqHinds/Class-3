e9dff21616229d38216c1ef40d13e93835375d03
TariqHinds <tkothunder@gmail.com>
2018-10-18 12:18:58 -0400

    Changes: gitignore applied.

---
+++ Assets/Player.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+	public Transform[] loseSpot;
+	public Transform winSpot;
+
+	public float gridSize = 2f;
+	Vector3 startPos;
+	// Use this for initialization
+	void Start () {
+		startPos = transform.position;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		Debug.Log("A is down:" + Input.GetKeyDown(KeyCode.A));
+	
+		if (Input.GetKeyDown (KeyCode.W) && transform.position.z < 0.1589975f) {
+			Debug.Log ("Player pressed w");
+			transform.position += transform.forward * gridSize; 
+		}
+		if (Input.GetKeyDown (KeyCode.A) && transform.position.x > -7.841001f) {
+		Debug.Log ("Player pressed a");
+		transform.position += -transform.right * gridSize; 
+		}
+		if (Input.GetKeyDown (KeyCode.S) && transform.position.z > -10.841002f) {
+			Debug.Log ("Player pressed s");
+			transform.position += -transform.forward * gridSize; 
+		}
+		if (Input.GetKeyDown (KeyCode.D) && transform.position.x < 0.1589975f) {
+			Debug.Log ("Player pressed d");
+			transform.position += transform.right * gridSize; 
+		}	
+		//A code that can stop the game object from moving off of the plane: If (Player.position > Plane.position) { Player.position < Plane.position;
+		//Spacebar reset of the game 
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			transform.position = startPos; 
+		}
+	}
+}
---
+++ Assets/Seek.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Seek : MonoBehaviour {
+	Rigidbody obj;
+	public Transform target;
+	// Use this for initialization
+	void Start () {
+		obj = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 direction = Vector3.Normalize(target.position - transform.position);
+		obj.AddForce (direction);
+	}
+}
