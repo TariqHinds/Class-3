ed022061a05ac14e3c49cd3ffb9038e5ae27dae5
TariqHinds <tkothunder@gmail.com>
2018-12-01 12:48:41 -0500

    Started the Luigiware design process and coding the win condition system. Micro game (Roll a ball) is 80% compleate so far.

---
+++ Assets/Scripts/KoolAgent.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KoolAgent : MonoBehaviour {
+	UnityEngine.AI.NavMeshAgent nav;
+	public Transform target;
+	// Use this for initialization
+	void Start () {
+		nav = GetComponent<UnityEngine.AI.NavMeshAgent> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+void OnTriggerStay(Collider col){
+		if (col.gameObject.CompareTag (bond)) {
+			nav.destination = col.target.position;
+		}
+	}
+}
